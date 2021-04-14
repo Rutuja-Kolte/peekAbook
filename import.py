c7a82943c5b352a0c30781fc6deaf0f4d17d9dd7
Rutuja Kolte <kolterutuja1@gmail.com>
2021-04-14 23:01:02 +0530

    review

--- import.py
+++ import.py
@@ -5,7 +5,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-engine = create_engine(os.getenv("DATABASE_URL"))
+engine = create_engine("postgresql://postgres:elephant@localhost:5432/Book_list")
 db = scoped_session(sessionmaker(bind=engine))
 
 def main():
